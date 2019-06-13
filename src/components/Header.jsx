1af58ae46eb1fe8d432605724fc7c516704fb8f2
Reznx <48242089+Reznx@users.noreply.github.com>
2019-06-13 17:44:29 +0500

    02:CSS-Modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className="header">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt="" />
+        <header className={s.header}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt="logo" className={s.img} />
         </header>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import s from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className="nav">
-            <ul>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Messages</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Music</a></li>
-                <li><a href="#">Settings</a></li>
+        <nav className={s.nav}>
+            <ul className={s.list}>
+                <li className={s.item}><a href="#" className={s.link}>Profile</a></li>
+                <li className={s.item}><a href="#" className={s.link}>Messages</a></li>
+                <li className={s.item}><a href="#" className={s.link}>News</a></li>
+                <li className={s.item}><a href="#" className={s.link}>Music</a></li>
+                <li className={s.item}><a href="#" className={s.link}>Settings</a></li>
             </ul>
         </nav>
     );
