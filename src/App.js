c1adb5f173ec499022138c21bf9e72368a5e2e1c
Reznx <48242089+Reznx@users.noreply.github.com>
2019-06-13 16:28:26 +0500

    02:New Structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from "react";
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt=""/>
-      </header>
-      <nav className="nav">
-        <ul>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Messages</a></li>
-          <li><a href="#">News</a></li>
-          <li><a href="#">Music</a></li>
-          <li><a href="#">Settings</a></li>
-        </ul>
-      </nav>
-      <div className="content">
-        <div className="content__img">
-          <img src="http://shapka-youtube.ru/wp-content/uploads/2018/10/beztexta-category.jpg" alt="Шапка"/>
-        </div>
-        <div>
-          {/* AVATAR + DESCRIPTION */}
-        </div>
-      </div>
+      <Header />
+
+      <Navbar />
+
+      <Profile />
     </div>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="content__img">
+                <img src="http://shapka-youtube.ru/wp-content/uploads/2018/10/beztexta-category.jpg" alt="Шапка" class="content__img" />
+            </div>
+            <div>
+                {/* AVATAR + DESCRIPTION */}
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
