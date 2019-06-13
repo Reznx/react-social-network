fe070a5caccdedd410c982dea1eca497d590f4ad
Reznx <48242089+Reznx@users.noreply.github.com>
2019-06-13 15:13:16 +0500

    Начинаем изучат ьреакт и делать социальную сеть

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt=""/>
       </header>
+      <nav className="nav">
+        <ul>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+      <div className="content">
+        <div className="content__img">
+          <img src="http://shapka-youtube.ru/wp-content/uploads/2018/10/beztexta-category.jpg" alt="Шапка"/>
+        </div>
+        <div>
+          {/* AVATAR + DESCRIPTION */}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+
 
 export default App;
