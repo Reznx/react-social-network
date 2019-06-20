5f287d734839b7719be46542311440390e23d323
Reznx <48242089+Reznx@users.noreply.github.com>
2019-06-20 17:11:59 +0500

    profilepic

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import s from "./Post.module.css";
+import img from "./user.png";
+
+
 
 const Post = (props) => {
   return (
     <div className={s.post}>
-      <img src="https://deti-online.com/img/raskraska-kot.jpg" alt="Avatar" />
+      <img src={img} alt="Avatar" />
       {props.message}
       <div>
-        <span>Like</span>{props.likesCount}
+        <span>Like</span>
+        {props.likesCount}
       </div>
     </div>
   );
