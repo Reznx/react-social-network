7511f3851e610483abd60e1cd19916b3a886779b
Reznx <48242089+Reznx@users.noreply.github.com>
2019-06-23 11:35:38 +0500

    GET response

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,27 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios";
+import userImg from "../../assets/img/user_no_img.png";
 
 let Users = props => {
-    if (props.users.length === 0) {
-        props.setUsers([
-          {
-            id: 1,
-            photoUrl:
-              "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-256.png",
-            followed: false,
-            fullName: "George",
-            status: "Hello everyone",
-            location: { city: "Chelyabinsk", country: "Russia" }
-          },
-          {
-            id: 2,
-            photoUrl:
-              "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-256.png",
-            followed: true,
-            fullName: "Evgeniy",
-            status: "Like a boss",
-            location: { city: "Paris", country: "Italy" }
-          },
-          {
-            id: 3,
-            photoUrl:
-              "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/8_avatar-256.png",
-            followed: true,
-            fullName: "Harold",
-            status: "Eee booooyy!",
-            location: { city: "Bangkok", country: "Thailand" }
-          }
-        ]);
-    }
-      
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        props.setUsers(response.data.items);
+      });
+  }
+
   return (
     <div>
       {props.users.map(u => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={styles.usersPhoto} />
+              <img
+                src={u.photos.small !== null ? u.photos.small : userImg}
+                className={styles.usersPhoto}
+              />
             </div>
             <div>
               {u.followed ? (
@@ -64,12 +45,12 @@ let Users = props => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
